65296a4b949ec8801ce755b0d7b8d3c4c089407b
Nicolas Ferreira <nicolas.ferreira12.nf@gmail.com>
2024-09-26 22:58:01 -0300

    fix: add functions implementation

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Dispositivo Nokia");
+Nokia nokia = new Nokia("5511911111111", "Nokia C21 Plus", "111111111", 128);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Instagram");
+
+Console.WriteLine("--------------------------------");
+Console.WriteLine("Dispositivo Iphone");
+Iphone iphone = new Iphone("5519922222222", "Iphone 15 Pro", "22222222", 512);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Facebook");
